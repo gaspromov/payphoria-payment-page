ec0e65bfc70edb70476d8dcf824f1f427877acbd
gaspromov <gaspromov@mail.ru>
2024-06-22 14:19:47 +0300

    [new] Icons font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pm-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'payment-page';
-}
+export class AppComponent {}
