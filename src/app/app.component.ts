4cee78fb3eecad35420ba87ba7c1cf9d904a1bbb
gaspromov <gaspromov@mail.ru>
2024-06-22 14:22:09 +0300

    [new] LazyLoadingSpinner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LazyLoadingSpinnerComponent } from './common/components/lazy-loading-spinner/lazy-loading-spinner.component';
 
 @Component({
   selector: 'pm-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, LazyLoadingSpinnerComponent],
+  template: ` <csd-lazy-loading-spinner /> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {}
---
+++ src/app/common/components/lazy-loading-spinner/lazy-loading-spinner.component.ts
@@ -0,0 +1,56 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { CommonModule } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'csd-lazy-loading-spinner',
+  template: `
+    <mat-progress-bar *ngIf="show()" mode="indeterminate" color="primary" />
+  `,
+  imports: [MatProgressBarModule, CommonModule],
+  standalone: true,
+  styles: `:host{
+    @apply tw-fixed tw-top-0 tw-left-0 tw-z-30 tw-w-full;
+  }`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LazyLoadingSpinnerComponent {
+  readonly show = signal(false);
+  readonly #destroyRef = inject(DestroyRef);
+
+  constructor(private router: Router) {
+    this.listenRouteLoading();
+  }
+
+  private listenRouteLoading() {
+    this.router.events
+      .pipe(takeUntilDestroyed(this.#destroyRef))
+      .subscribe((event) => {
+        if (event instanceof NavigationStart) {
+          this.show.set(true);
+        } else if (this.show() == false) {
+          return;
+        } else if (
+          event instanceof NavigationEnd ||
+          event instanceof NavigationCancel ||
+          event instanceof NavigationError
+        ) {
+          this.show.set(false);
+        }
+      });
+  }
+}
