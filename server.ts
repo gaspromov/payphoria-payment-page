23f64e8ba1e2318a64c680b5a01bc9805af6260b
gaspromov <gaspromov@mail.ru>
2024-06-22 14:02:53 +0300

    [new] Compression for SSR

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import compression from 'compression';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -14,15 +15,21 @@ export function app(): express.Express {
 
   const commonEngine = new CommonEngine();
 
+  // Compress all HTTP responses
+  server.use(compression());
+
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(browserDistFolder, {
-    maxAge: '1y'
-  }));
+  server.get(
+    '*.*',
+    express.static(browserDistFolder, {
+      maxAge: '1y',
+    })
+  );
 
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
