8e737bdbf6afad46b217d0ed9f81e097d93a2ad5
gaspromov <gaspromov@mail.ru>
2024-06-28 18:07:33 +0300

    [fix] Order timer fixed

--- src/app/order/components/order-timer.component.ts
+++ src/app/order/components/order-timer.component.ts
@@ -1,36 +1,81 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  DestroyRef,
+  OnDestroy,
+  OnInit,
   PLATFORM_ID,
   inject,
+  signal,
 } from '@angular/core';
 import { Store } from '@pm-store/store';
-import { filter, interval, map, switchMap } from 'rxjs';
-import { differenceInMilliseconds } from 'date-fns';
-import { AsyncPipe, DatePipe, isPlatformServer } from '@angular/common';
+import {
+  Subject,
+  distinctUntilChanged,
+  filter,
+  interval,
+  map,
+  switchMap,
+  takeUntil,
+  tap,
+} from 'rxjs';
+import { differenceInMilliseconds, isBefore } from 'date-fns';
+import { DatePipe, isPlatformBrowser } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'pm-order-timer',
   template: `
     <p>
       Времени на оплату:
-      {{ timeLeft$ | async | date : 'mm:ss' }}
+      {{ timeLeft() | date : 'mm:ss' }}
     </p>
   `,
   styles: `p{ @apply tw-text-success tw-font-bold tw-text-xl; }`,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [AsyncPipe, DatePipe],
+  imports: [DatePipe],
   standalone: true,
 })
-export class OrderTimerComponent {
-  readonly #platformId = inject(PLATFORM_ID);
+export class OrderTimerComponent implements OnInit, OnDestroy {
+  readonly timeLeft = signal<number | null>(0);
 
-  readonly timeLeft$ = inject(Store)
-    .order.selectData()
-    .pipe(
-      map((order) => order.expiresAt!),
-      filter(() => !isPlatformServer(this.#platformId)),
-      switchMap((expiresAt) => interval(1000).pipe(map(() => expiresAt))),
-      map((expiresAt) => differenceInMilliseconds(expiresAt, new Date()))
+  readonly #platformId = inject(PLATFORM_ID);
+  readonly #intervalDestroyer$ = new Subject<void>();
+
+  constructor(private store: Store, private snBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+    this.store.order
+      .selectData()
+      .pipe(
+        takeUntil(this.#intervalDestroyer$),
+        map((order) => order.expiresAt!),
+        distinctUntilChanged(),
+        tap((expiresAt) => this.setTimeLeft(expiresAt)),
+        filter(() => isPlatformBrowser(this.#platformId)),
+        switchMap((expiresAt) => interval(1000).pipe(map(() => expiresAt))),
+        tap((d) => console.log(d))
+      )
+      .subscribe((expiresAt) => this.setTimeLeft(expiresAt));
+  }
+
+  ngOnDestroy(): void {
+    this.#intervalDestroyer$.next();
+    this.#intervalDestroyer$.complete();
+  }
+
+  private setTimeLeft(expiresAt: string) {
+    if (isBefore(new Date(expiresAt), new Date())) {
+      this.timeLeft.set(0);
+      this.#intervalDestroyer$.next();
+      this.snBar.open('Заказ просрочен');
+      return;
+    }
+    const difference = differenceInMilliseconds(
+      new Date(expiresAt),
+      new Date()
     );
+    this.timeLeft.set(difference);
+  }
 }
